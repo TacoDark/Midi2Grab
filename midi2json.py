9650a646cbd877970ceea780d32ac70232160ae3
TacoDark <ryanandkain@gmail.com>
2025-08-10 13:05:49 -0500

    Got basic functionality working, it correctly makes the sound blocks in position, but it doesnt generate the sound p-roperties correctly.

---
+++ midi2json.py
@@ -0,0 +1,111 @@
+import os
+import json
+from mido import MidiFile
+
+def midi_note_to_freq(note):
+    return 440.0 * 2 ** ((note - 69) / 12)
+
+def get_template_for_instrument(instrument_name):
+    mapping = {
+        'piano': 'synth.json',
+        'synth': 'synth.json',
+        'lead': 'synth.json',
+        'bells': 'blipSelect.json',
+        'bass': 'pickupCoin.json',
+        'kick': 'explosion.json',
+        'snare': 'hitHurt.json',
+        'hihat': 'click.json',
+        'cymbal': 'mutate.json',
+        'clap': 'hitHurt.json',
+        'tom': 'hitHurt.json',
+    }
+    for key in mapping:
+        if key in instrument_name.lower():
+            return mapping[key]
+    return 'synth.json'
+
+def load_template(template_dir, template_file):
+    path = os.path.join(template_dir, template_file)
+    with open(path, 'r') as f:
+        return json.load(f)
+
+def midi_to_json(midi_path, template_dir, output_path):
+    midi = MidiFile(midi_path)
+    level_nodes = []
+    sound_id = 1
+    for i, track in enumerate(midi.tracks):
+        instrument_name = track.name if track.name else 'synth'
+        template_file = get_template_for_instrument(instrument_name)
+        template = load_template(template_dir, template_file)
+        current_time = 0
+        for msg in track:
+            if msg.type == 'note_on' and msg.velocity > 0:
+                freq = midi_note_to_freq(msg.note)
+                current_time += msg.time
+                y_pos = current_time
+                sound_parameters = {}
+                for k, v in template.items():
+                    if k == "p_base_freq":
+                        sound_parameters["frequencyBase"] = freq
+                    elif k == "p_freq_limit":
+                        sound_parameters["frequencyLimit"] = freq
+                    elif k == "p_freq_ramp":
+                        sound_parameters["frequencyRamp"] = v
+                    elif k == "p_freq_dramp":
+                        sound_parameters["frequencyDeltaRamp"] = v
+                    elif k == "p_env_punch":
+                        sound_parameters["pitchJumpMod"] = v
+                    elif k == "p_duty_ramp":
+                        sound_parameters["dutyCycleRamp"] = v
+                    elif k == "p_pha_offset":
+                        sound_parameters["flangerFrequency"] = v
+                    elif k == "p_hpf_freq":
+                        sound_parameters["highPassFilterFrequency"] = v
+                node = {
+                    'levelNodeSound': {
+                        'position': {'x': 0, 'y': y_pos, 'z': 0},
+                        'parameters': sound_parameters,
+                        'name': instrument_name,
+                        'volume': -1,
+                        'maxRangeFactor': 1,
+                        'rotation': {'x': 0, 'y': 0, 'z': 0, 'w': 1},
+                        'scale': {'x': 1, 'y': 1, 'z': 1}
+                    }
+                }
+                level_nodes.append(node)
+                trigger = {
+                    'levelNodeTrigger': {
+                        'shape': 1001,
+                        'position': {'x': 0, 'y': y_pos, 'z': 0},
+                        'scale': {'x': 1, 'y': 1, 'z': 1},
+                        'rotation': {'x': 0, 'y': 0, 'z': 0, 'w': 1},
+                        'isShared': True,
+                        'triggerSources': [{'triggerSourceBasic': {'type': 4}}],
+                        'triggerTargets': [{'triggerTargetSound': {'objectID': sound_id, 'mode': 3}}]
+                    }
+                }
+                level_nodes.append(trigger)
+                sound_id += 1
+    result = {
+        'formatVersion': 15,
+        'title': 'sound',
+        'complexity': 13,
+        'maxCheckpointCount': 10,
+        'ambienceSettings': {
+            'skyZenithColor': {'r': 0.28, 'g': 0.476, 'b': 0.73, 'a': 1},
+            'skyHorizonColor': {'r': 0.916, 'g': 0.9574, 'b': 0.9574, 'a': 1},
+            'sunAltitude': 45,
+            'sunAzimuth': 315,
+            'sunSize': 1
+        },
+        'levelNodes': level_nodes
+    }
+    with open(output_path, 'w') as f:
+        json.dump(result, f, indent=4)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print('Usage: python midi2json.py <input.mid> <output.json>')
+        sys.exit(1)
+    midi_to_json(sys.argv[1], 'templates', sys.argv[2])
